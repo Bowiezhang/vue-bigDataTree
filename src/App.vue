4ade2034350dd23fab996449c7a21a5cfdb5df98
zhangjiexiang <zhangjiexiang@tpson.cn>
2020-06-08 10:31:42 +0800

    init：初始化项目

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <div class="content">
+      <el-tree
+        :props="props"
+        :load="loadNode"
+        lazy
+        show-checkbox>
+      </el-tree>
+    </div>
+    <div class="content">
+      <big-data-tree></big-data-tree>
+    </div>
+  </div>
+</template>
+
+<script>
+import BigDataTree from '@/components/big-data-tree.vue'
+
+export default {
+  name: 'App',
+
+  components: { BigDataTree },
+
+  data() {
+    return {
+      props: {
+        label: 'name',
+        children: 'children',
+        disabled: 'disabled',
+        isLeaf: 'leaf'
+      },
+    }
+  },
+
+  methods: {
+    loadNode(node, resolve) {
+      console.log(resolve)
+      const list = []
+      for (let index = 0; index < 30; index++) {
+        list[index] = { name: `节点${index}` }
+      }
+      
+      return resolve(list)
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: calc(100% - 100px);
+  width: calc(100% - 100px);
+  padding: 50px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.content {
+  width: 30%;
+  height: 100%;
+  overflow: auto;
+}
+</style>
---
+++ src/components/big-data-tree.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="big-data-tree">
+    <div class="tree-item">
+      <div class="tree-content">
+        <div class="node-expand"></div>
+        <input class="node-checkbox" type="checkbox">
+        <div class="node-detail">ceshi</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+
+  props: {
+    props: {
+      type: Object,
+      default: () => {
+        return {
+          label: 'name',
+          children: 'children',
+          isLeaf: 'leaf'
+        }
+      }
+    },
+
+    load: {
+      type: Function,
+      default: () => {}
+    },
+
+    showCheckbox: {
+      type: Boolean,
+      default: false,
+    }
+  },
+
+  data() {
+    return {
+      treeList: []
+    }
+  },
+
+  methods: {
+    init() {
+      const initNode = {}
+    }
+  },
+
+  mounted() {}
+}
+</script>
+
+<style scoped>
+.big-data-tree {
+  position: relative;
+  cursor: default;
+  background: #FFF;
+  color: #606266;
+}
+
+.tree-item {
+  height: 26px;
+  white-space: nowrap;
+  outline: 0;
+}
+
+.tree-content {
+  height: 26px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+}
+
+.tree-content:hover {
+  background-color: #F5F7FA;
+}
+
+.node-expand {
+  position: relative;
+  padding: 12px;
+}
+
+.node-expand::before {
+  content: '';
+  width: 0;
+  height: 0;
+  border: 4px solid transparent;
+  border-left: 4px solid #C0C4CC;
+  position: absolute;
+  top: 8px;
+  left: 10px;
+}
+
+.node-checkbox {
+  cursor: pointer;
+  margin-right: 8px;
+}
+
+.node-detail {
+  flex: 1;
+}
+</style>
